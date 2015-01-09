ba462b383aeee8f3edfa69a33cf5279b8af16d85
Luiz Filipe Martins Ramos <t-luiram@microsoft.com>
2015-01-09 14:19:17 -0800

    Added a sample index.html, created assets bundle and changed services path to /services/*

--- src/main/java/com/yammer/stresstime/StresstimeApplication.java
+++ src/main/java/com/yammer/stresstime/StresstimeApplication.java
@@ -3,14 +3,23 @@ package com.yammer.stresstime;
 import com.yammer.stresstime.config.StresstimeConfiguration;
 import com.yammer.stresstime.resources.HelloWorldResource;
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class StresstimeApplication extends Application<StresstimeConfiguration> {
     public static void main(String[] args) throws Exception {
         new StresstimeApplication().run(args);
     }
+
     @Override
-    public void run(StresstimeConfiguration stresstimeConfiguration, Environment environment) throws Exception {
+    public void initialize(Bootstrap<StresstimeConfiguration> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/assets/", "/", "index.html"));
+    }
+
+    @Override
+    public void run(StresstimeConfiguration configuration, Environment environment) throws Exception {
+        environment.jersey().setUrlPattern(configuration.getRootPath());
         environment.jersey().register(new HelloWorldResource());
     }
 }
--- src/main/java/com/yammer/stresstime/config/StresstimeConfiguration.java
+++ src/main/java/com/yammer/stresstime/config/StresstimeConfiguration.java
@@ -1,7 +1,17 @@
 package com.yammer.stresstime.config;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 
 public class StresstimeConfiguration extends Configuration {
 
+    private final String rootPath;
+
+    public StresstimeConfiguration(@JsonProperty("rootPath") String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+    public String getRootPath() {
+        return rootPath;
+    }
 }
