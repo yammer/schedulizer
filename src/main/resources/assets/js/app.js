5aea67ff8f3fbbb3573beafe153b2eac4e93b59e
Luiz Filipe Martins Ramos <t-luiram@microsoft.com>
2015-01-12 11:00:08 -0800

    Added a navbar with 3 tabs: Groups, My Calendar and Availability

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -6,5 +6,9 @@ var App = angular.module('StresstimeApp', ['ui.bootstrap', 'ngRoute']);
 
 
 App.controller("MainController", function($scope, $http, $log, $timeout, $modal) {
-    $scope.hello = "HelloWorld";
+    $scope.GROUPS = "groups";
+    $scope.CALENDAR = "calendar";
+    $scope.AVAILABILITY = "availability";
+    $scope.groupList = [{name: "group1"}, {name: "group2"}];
+    $scope.hello='ola';
 });
