f9cd16dd1afb9c634611ab586ca41e6b48a62d90
Luiz Filipe Martins Ramos <t-luiram@microsoft.com>
2015-01-12 11:17:17 -0800

    Added groupList to the navbar

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -6,9 +6,7 @@ var App = angular.module('StresstimeApp', ['ui.bootstrap', 'ngRoute']);
 
 
 App.controller("MainController", function($scope, $http, $log, $timeout, $modal) {
-    $scope.GROUPS = "groups";
-    $scope.CALENDAR = "calendar";
-    $scope.AVAILABILITY = "availability";
+    $scope.CALENDAR = "CALENDAR";
+    $scope.AVAILABILITY = "AVAILABILITY";
     $scope.groupList = [{name: "group1"}, {name: "group2"}];
-    $scope.hello='ola';
 });
