f9ab4ef87d32fd553aa3cca709800f7acec8d2bc
Bernardo Rufino <t-beruf@microsoft.com>
2015-01-14 17:51:35 -0800

    Add test for DayRestriction
    Add tests for its AssignmentType association.

---
+++ src/test/java/com/yammer/stresstime/entities/DayRestrictionTest.java
@@ -0,0 +1,72 @@
+package com.yammer.stresstime.entities;
+
+import com.google.common.collect.ImmutableSet;
+import com.yammer.stresstime.managers.AssignmentTypeManager;
+import com.yammer.stresstime.managers.DayRestrictionManager;
+import com.yammer.stresstime.test.DatabaseTest;
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.Assert.assertNotNull;
+
+public class DayRestrictionTest extends DatabaseTest {
+
+    private DayRestrictionManager mDayRestrictionManager;
+    private AssignmentTypeManager mAssignmentTypeManager;
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mDayRestrictionManager = new DayRestrictionManager(getSessionFactory());
+        mAssignmentTypeManager = new AssignmentTypeManager(getSessionFactory());
+    }
+
+    @Test
+    public void testHasEmptyAssignmentTypesAfterInstantiated() {
+        DayRestriction restriction = new DayRestriction();
+
+        assertNotNull(restriction.getAssignmentTypes());
+        assertThat(restriction.getAssignmentTypes(), empty());
+    }
+
+    @Test
+    public void testHasEmptyAssignmentTypesAfterRetrievedFromDb() {
+        DayRestriction restriction = new DayRestriction();
+        restriction.setDate(new LocalDate(2015, 3, 23));
+        mDayRestrictionManager.save(restriction);
+        refresh(restriction);
+
+        assertNotNull(restriction.getAssignmentTypes());
+        assertThat(restriction.getAssignmentTypes(), empty());
+
+        mDayRestrictionManager.delete(restriction);
+    }
+
+    @Test
+    public void testRetrieveAssignmentTypeAfterBeingAdded() {
+        AssignmentType type = new AssignmentType();
+        type.setName("Primary");
+        mAssignmentTypeManager.save(type);
+
+        DayRestriction restriction = new DayRestriction();
+        restriction.setDate(new LocalDate(2015, 3, 23));
+        Set<AssignmentType> assignmentTypes = new ImmutableSet.Builder<AssignmentType>()
+                .addAll(restriction.getAssignmentTypes())
+                .add(type).build();
+        restriction.setAssignmentTypes(assignmentTypes);
+        mDayRestrictionManager.save(restriction);
+
+        refresh(type, restriction);
+
+        Set<AssignmentType> types = restriction.getAssignmentTypes();
+        Assert.assertThat(types, hasItems(type));
+    }
+}
