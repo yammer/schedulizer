da67f67b3c363ed6161151cbd2cf78b59cbcfbda
Luiz Filipe Martins Ramos <t-luiram@microsoft.com>
2015-01-15 15:41:23 -0800

    Created infinite scrolling for the calendar! :)

--- src/main/resources/assets/js/controllers/calendar.js
+++ src/main/resources/assets/js/controllers/calendar.js
@@ -14,44 +14,91 @@ function computeNWeeks(firstDay) {
     return Math.ceil(diff/7.0);
 }
 
+function initWeek(day){
+    var currentWeek = {};
+    currentWeek.month = {
+        begins: false, // to be filled
+        name: day.getMonthName(),
+        nweeks: 0 // to be filled in case month begins
+    };
+    currentWeek.days = [];
+    return currentWeek;
+}
+
+function initMonth(day01) {
+    var month = {};
+    month.name = day01.getMonthName();
+    month.begins = true;
+    month.nweeks = computeNWeeks(new Date(day01.getTime()));
+    return month;
+}
+
 App.controller('CalendarController', function ($scope) {
     $scope.calendar = [];
-    console.log("prototyping");
     Date.prototype.getMonthName = function() {
         var monthNames = [ "January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December" ];
         return monthNames[this.getMonth()];
     };
 
-    var today = new Date();
-    var weekday = today.getDay();
-    var lastDay = new Date();
-    var firstDay = new Date();
-    lastDay.setDate(today.getDate() - 7*7 - today.getDay()); /* guaranteed to be Sunday */
-    firstDay.setDate(today.getDate() - 50);
-    var firstMonthStarted = false;
-    for(var weekCount = 0; weekCount < 25; weekCount++) {
-        var currentWeek = {};
-        currentWeek.month = {
-            begins: false, // to be filled
-            name: lastDay.getMonthName(),
-            nweeks: 0 // to be filled in case month begins
-        };
-        currentWeek.days = [];
-        for(var dayCount = 0; dayCount < 7; dayCount++){
-            if(lastDay.getDate() == 1) {
-                firstMonthStarted = true;
-                currentWeek.month.name = lastDay.getMonthName();
-                currentWeek.month.begins = true;
-                currentWeek.month.nweeks = computeNWeeks(new Date(lastDay.getTime()));
+    $scope.firstDay = new Date();
+    $scope.firstDay.setDate(1); // beginning of the month
+    $scope.firstDay.setDate($scope.firstDay.getDate() - $scope.firstDay.getDay()); // sunday
+    $scope.lastDay = new Date($scope.firstDay.getTime())
+
+    $scope.loadPreviousMonth = function() {
+        var isBegginingOfMonth = false;
+        do {
+            $scope.firstDay.setDate($scope.firstDay.getDate() - 7);
+            var currentWeek = initWeek($scope.firstDay);
+            for(var dayCount = 0; dayCount < 7; dayCount++) {
+                if($scope.firstDay.getDate() == 1) {
+                    isBegginingOfMonth = true;
+                    currentWeek.month = initMonth($scope.firstDay);
+                }
+                currentWeek.days.push({ value: $scope.firstDay.getDate() });
+                $scope.firstDay.setDate($scope.firstDay.getDate() + 1);
             }
-            currentWeek.days.push({value: lastDay.getDate()});
-            lastDay.setDate(lastDay.getDate() + 1);
-        }
-        if(firstMonthStarted) {
-            $scope.calendar.push(currentWeek);
-        }
+            $scope.calendar.unshift(currentWeek);
+            $scope.firstDay.setDate($scope.firstDay.getDate() - 7);
+        } while(!isBegginingOfMonth);
+        // TODO: CHANGE THE HARD CODED 75px AND STOP USING JQUERY (FIND A BETTER WAY)
+        var elemHeight = $(".calendarcell").outerHeight();
+        var h = (elemHeight > 0) ? elemHeight : 75;
+        return h * $scope.calendar[0].month.nweeks; // size of the chunk
     }
+
+    $scope.loadNextMonth = function() {
+        var isBegginingOfMonth = false;
+        if($scope.calendar.length > 0 ) {
+            $scope.calendar[$scope.calendar.length - 1].month.name = $scope.lastDay.getMonthName();
+        }
+        do {
+            var currentWeek = initWeek($scope.lastDay);
+            for(var dayCount = 0; dayCount < 7; dayCount++) {
+                if($scope.lastDay.getDate() == 1) {
+                    isBegginingOfMonth = true;
+                    currentWeek.month = initMonth($scope.lastDay);
+                }
+                currentWeek.days.push({ value: $scope.lastDay.getDate() });
+                $scope.lastDay.setDate($scope.lastDay.getDate() + 1);
+            }
+            $scope.calendar.push(currentWeek);
+        } while(!isBegginingOfMonth);
+        $scope.calendar[$scope.calendar.length - 1].month.name = "";
+    }
+
+    $scope.initCalendar = function() {
+        $scope.loadNextMonth();
+        $scope.loadNextMonth();
+        $scope.loadNextMonth();
+        $scope.loadPreviousMonth();
+        $scope.loadPreviousMonth();
+    }
+
+    $scope.initCalendar();
+
+
 });
 
 App.directive('calendar', function() {
@@ -59,5 +106,39 @@ App.directive('calendar', function() {
         restrict: 'E',
         templateUrl: 'html/partials/calendar.html',
         controller: 'CalendarController'
+    };
+});
+App.directive('scroller', function () {
+    return {
+        restrict: 'A',
+        scope: {
+            nextChunkMethod: "&",
+            previousChunkMethod: "&"
+        },
+        link: function ($scope, elem, attrs) {
+            rawElement = elem[0];
+
+            elem.bind('scroll', function () {
+                if((rawElement.scrollTop + rawElement.offsetHeight+5) >= rawElement.scrollHeight &&
+                    $scope.nextChunkMethod){
+                    $scope.$apply($scope.nextChunkMethod);
+                }
+                if(rawElement.scrollTop == 0 &&
+                    $scope.previousChunkMethod){
+                    $scope.$apply(function() {
+                        var chunkSize = $scope.previousChunkMethod();
+                        if(chunkSize) {
+                            rawElement.scrollTop = chunkSize;
+                        }
+                        else {
+                            setTimeout(function () {
+                                rawElement.scrollTop = rawElement.scrollHeight - previousHeight;
+                            }, 0);
+                        }
+                    });
+
+                }
+            });
+        }
     };
 });
