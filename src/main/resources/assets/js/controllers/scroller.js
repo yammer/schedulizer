81323bda29203daf47f26d76c4af97bdf2cc977f
Bernardo Rufino <t-beruf@microsoft.com>
2015-01-19 15:16:43 -0800

    Nitpicks and comments on scroller.js

--- src/main/resources/assets/js/controllers/scroller.js
+++ src/main/resources/assets/js/controllers/scroller.js
@@ -3,34 +3,37 @@ App.directive('scroller', function () {
         restrict: 'A',
         scope: {
             nextChunkMethod: "&",
-            previousChunkMethod: "&",
+            previousChunkMethod: "&", // return the height of the chunk if you don't want a flash on the screen
             numberOfLoadedChunksPerScroll: "="
         },
-        link: function ($scope, elem, attrs) {
-            rawElement = elem[0];
+        // Make scroll even if it does not have scroll (increase height to test)
+        link: function($scope, element, attrs) {
+            rawElement = element[0];
             if ($scope.numberOfLoadedChunksPerScroll == undefined) {
                 $scope.numberOfLoadedChunksPerScroll = 1;
             }
-            elem.bind('scroll', function () {
-                if((rawElement.scrollTop + rawElement.offsetHeight+5) >= rawElement.scrollHeight &&
-                    $scope.nextChunkMethod){
-                    $scope.$apply( function() {
+            element.bind('scroll', function () {
+                // + 5 error margin
+                if ((rawElement.scrollTop + rawElement.offsetHeight + 5) >= rawElement.scrollHeight
+                        && $scope.nextChunkMethod) {
+                    // User has scrolled down and we should fetch next chunk
+                    $scope.$apply(function() {
                         for(var i = 0; i < $scope.numberOfLoadedChunksPerScroll; i++) {
                             $scope.nextChunkMethod();
                         }
                     });
                 }
-                if(rawElement.scrollTop == 0 &&
-                    $scope.previousChunkMethod){
+                if (rawElement.scrollTop == 0 && $scope.previousChunkMethod){
+                    // User has scrolled up and we should fetch previous chunk
                     $scope.$apply(function() {
                         var chunkSize = 0;
                         for(var i = 0; i < $scope.numberOfLoadedChunksPerScroll; i++) {
                             chunkSize += $scope.previousChunkMethod();
                         }
-                        if(chunkSize) {
+                        if (chunkSize) { // If the provided function gave us the height, GOOD
                             rawElement.scrollTop = chunkSize;
-                        }
-                        else {
+                        } else { // Otherwise we have to calculate and flash the screen =(
+                            var previousHeight = rawElement.scrollHeight;
                             setTimeout(function () {
                                 rawElement.scrollTop = rawElement.scrollHeight - previousHeight;
                             }, 0);
