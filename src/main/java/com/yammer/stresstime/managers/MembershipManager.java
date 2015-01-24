480dc598fddc63d0762db1d03e47e2628f7638e8
Bernardo Rufino <t-beruf@microsoft.com>
2015-01-23 16:12:18 -0800

    EntityNonUniqueException created

--- src/main/java/com/yammer/stresstime/managers/MembershipManager.java
+++ src/main/java/com/yammer/stresstime/managers/MembershipManager.java
@@ -3,6 +3,9 @@ package com.yammer.stresstime.managers;
 import com.yammer.stresstime.entities.Employee;
 import com.yammer.stresstime.entities.Group;
 import com.yammer.stresstime.entities.Membership;
+import com.yammer.stresstime.managers.exceptions.EntityNonUniqueException;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 
@@ -19,15 +22,18 @@ public class MembershipManager extends EntityManager<Membership> {
         return membership;
     }
 
-
-
     public Membership getByEmployeeIdAndGroupId(long employeeId, long groupId) {
-        Membership membership = (Membership) currentSession()
+        Criteria criteria = currentSession()
                 .createCriteria(Membership.class)
                 .add(Restrictions.eq("mEmployee.mId", employeeId))
-                .add(Restrictions.eq("mGroup.mId", groupId))
-                .uniqueResult();
-        return membership;
+                .add(Restrictions.eq("mGroup.mId", groupId));
+        try {
+            return (Membership) criteria.uniqueResult();
+        } catch (HibernateException e) {
+            EntityNonUniqueException error = new EntityNonUniqueException(Membership.class);
+            error.initCause(e);
+            throw error;
+        }
     }
 
 }
