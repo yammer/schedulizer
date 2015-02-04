edd8e6f7be1dd85cc4f2d60e26796b2f66ee94a0
Bernardo Rufino <t-beruf@microsoft.com>
2015-02-03 21:47:26 -0800

    Fix tests by creating test suite
    Tests weren't running together because the application had to be reset
    every time a test class was loaded. Every test is grouped in a test suite
    now, using the ClasspathSuite runner, that scans for test classes.
    Dependency was adjusted to include junit as well as cpsuite, which contains
    ClasspathSuite.

--- src/test/java/com/yammer/stresstime/entities/AssignmentTypeTest.java
+++ src/test/java/com/yammer/stresstime/entities/AssignmentTypeTest.java
@@ -1,6 +1,12 @@
 package com.yammer.stresstime.entities;
 
 import com.yammer.stresstime.test.DatabaseTest;
+import org.junit.Test;
 
 public class AssignmentTypeTest extends DatabaseTest {
+
+    @Test
+    public void testStub() throws Exception {
+        /* TODO  */
+    }
 }
