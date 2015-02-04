f905d79904ea40146d26a7c01a460154c5f74aac
Luiz Filipe Martins Ramos <t-luiram@microsoft.com>
2015-02-04 13:57:53 -0800

    Cleaning main controller

--- src/main/resources/assets/js/controllers/main_controller.js
+++ src/main/resources/assets/js/controllers/main_controller.js
@@ -1,6 +1,5 @@
 App.controller("MainController", function(NAV_TABS, $scope, $http, $timeout, $location) {
     /* injecting constants into scope */
-    // $route.routes contains the elements of NAV_TABS augmented
     $scope.tabs = angular.copy(NAV_TABS);
 
     $scope.isActiveTab = function(tab) {
@@ -10,33 +9,4 @@ App.controller("MainController", function(NAV_TABS, $scope, $http, $timeout, $lo
             return false
         }
     }
-
-    /* defining constants */
-    $scope.STATUS = { GUEST: 'guest', USER: 'user', ADMIN: 'admin', GLOBALADMIN: 'globaladmin'}
-
-    /* main functionality */
-
-
-        // <TODO: Extract authorization logic to the server!>
-
-//        $scope.userStatus = $scope.STATUS.GUEST;
-//
-//        yammer.getLoginStatus(function(response) {
-//            $timeout(function() {
-//                if (response.authResponse) {
-//                    $scope.userStatus = $scope.STATUS.GLOBALADMIN;
-//                    console.dir(response); //print user information to the console
-//                }
-//                console.log('TODO: change tabs');
-//            }, 0);
-//        });
-//
-//        $scope.isLogged = function() { return $scope.userStatus != $scope.STATUS.GUEST; } ;
-//        $scope.isAdmin = function() {
-//            return $scope.userStatus == $scope.STATUS.ADMIN || $scope.userStatus == $scope.STATUS.GLOBALADMIN;
-//        };
-//
-
-
-        // </TODO>
 });
