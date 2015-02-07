d149cbc59fd48db45a2cd4635be48677de7551f9
Bernardo Rufino <t-beruf@microsoft.com>
2015-02-06 16:09:04 -0800

    Update EntityManager with useful methods and remove GroupManager random
    Methods such as getExactOne, getUnique and checkFound that can work on
    entities of the own manager and whichever entities given in the parameters
    (by its class).

--- src/main/java/com/yammer/stresstime/managers/GroupManager.java
+++ src/main/java/com/yammer/stresstime/managers/GroupManager.java
@@ -1,26 +1,11 @@
 package com.yammer.stresstime.managers;
 
 import com.yammer.stresstime.entities.Group;
-import com.yammer.stresstime.managers.exceptions.EntityNotFoundException;
 import org.hibernate.SessionFactory;
-
-import java.util.List;
-import java.util.Random;
 
 public class GroupManager extends EntityManager<Group> {
 
-    private static final Random RANDOM = new Random();
-
     public GroupManager(SessionFactory sessionFactory) {
         super(sessionFactory, Group.class);
-    }
-
-    public Group random() {
-        /* TODO: Hit db directly */
-        List<Group> groups = all();
-        if (groups.size() < 1) {
-            throw new EntityNotFoundException(Group.class);
-        }
-        return groups.get(RANDOM.nextInt(groups.size()));
     }
 }
