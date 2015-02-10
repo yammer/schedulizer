670a45657dded1598b263d7b03d3a336e0842ebf
Luiz Filipe Martins Ramos <t-luiram@microsoft.com>
2015-02-09 16:13:52 -0800

    Add group admin information to '/current' request

--- src/main/java/com/yammer/stresstime/resources/AuthorizationResource.java
+++ src/main/java/com/yammer/stresstime/resources/AuthorizationResource.java
@@ -2,6 +2,8 @@ package com.yammer.stresstime.resources;
 
 import com.yammer.stresstime.auth.Authorize;
 import com.yammer.stresstime.auth.Role;
+import com.yammer.stresstime.entities.Group;
+import com.yammer.stresstime.entities.Membership;
 import com.yammer.stresstime.entities.User;
 import io.dropwizard.hibernate.UnitOfWork;
 
@@ -11,7 +13,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Path("/current")
 @Produces(MediaType.APPLICATION_JSON)
@@ -26,6 +30,12 @@ public class AuthorizationResource {
         response.put("role", user.getRole().toString());
         if (user.getEmployee() != null) {
             response.put("employeeId", user.getEmployee().getId());
+            List<Long> groupIds = user.getEmployee().getMemberships().stream()
+                    .filter(m -> m.isAdmin())
+                    .map(Membership::getGroup)
+                    .map(Group::getId)
+                    .collect(Collectors.toList());
+            response.put("groupsAdmin", groupIds);
         }
 
         return Response.ok().entity(response).build();
