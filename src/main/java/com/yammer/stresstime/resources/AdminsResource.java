48c54b6df15ccefe79ae5eeed661055f3aaf2ce0
Luiz Filipe Martins Ramos <t-luiram@microsoft.com>
2015-02-09 18:03:23 -0800

    Create admins resource to handle creation or deletion of admin status

---
+++ src/main/java/com/yammer/stresstime/resources/AdminsResource.java
@@ -0,0 +1,66 @@
+package com.yammer.stresstime.resources;
+
+import com.yammer.stresstime.auth.Authorize;
+import com.yammer.stresstime.auth.Role;
+import com.yammer.stresstime.entities.Employee;
+import com.yammer.stresstime.entities.Group;
+import com.yammer.stresstime.entities.Membership;
+import com.yammer.stresstime.entities.User;
+import com.yammer.stresstime.managers.EmployeeManager;
+import com.yammer.stresstime.managers.GroupManager;
+import com.yammer.stresstime.managers.MembershipManager;
+import com.yammer.stresstime.utils.ResourceUtils;
+import io.dropwizard.hibernate.UnitOfWork;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Path("/admins")
+@Produces(MediaType.APPLICATION_JSON)
+public class AdminsResource {
+
+    private GroupManager groupManager;
+    private MembershipManager membershipManager;
+
+    public AdminsResource(GroupManager groupManager, MembershipManager membershipManager) {
+        this.groupManager = groupManager;
+        this.membershipManager = membershipManager;
+    }
+
+    @POST
+    @UnitOfWork
+    public Response addAdmin(
+            @Authorize({Role.ADMIN, Role.MEMBER}) User user,
+            @FormParam("group_id") long groupId,
+            @FormParam("employee_id") long employeeId) {
+
+        Group group = groupManager.getById(groupId);
+        ResourceUtils.checkGroupAdminOrGlobalAdmin(group, user.getEmployee());
+        Membership membership = membershipManager.getByEmployeeIdAndGroupId(employeeId, groupId);
+        ResourceUtils.checkConflictFree(membership != null, Membership.class);
+        membership.setAdmin(true);
+        membershipManager.save(membership);
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @UnitOfWork
+    public Response removeAdmin(
+            @Authorize({Role.ADMIN, Role.MEMBER}) User user,
+            @FormParam("group_id") long groupId,
+            @FormParam("employee_id") long employeeId) {
+
+        Group group = groupManager.getById(groupId);
+        ResourceUtils.checkGroupAdminOrGlobalAdmin(group, user.getEmployee());
+        Membership membership = membershipManager.getByEmployeeIdAndGroupId(employeeId, groupId);
+        ResourceUtils.checkConflictFree(membership != null, Membership.class);
+        membership.setAdmin(false);
+        membershipManager.save(membership);
+        return Response.ok().build();
+    }
+}
