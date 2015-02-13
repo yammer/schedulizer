715c89b460dcb816cc1ce2eddb0e586d1ac966dd
Luiz Filipe Martins Ramos <t-luiram@microsoft.com>
2015-02-13 10:32:34 -0800

    Add debouce to window resize event

--- src/main/resources/assets/js/ext/dynamic_css.js
+++ src/main/resources/assets/js/ext/dynamic_css.js
@@ -1,3 +1,4 @@
+var count=0;
 App.run(function($window, $timeout, $rootScope) {
     function findNextParent(el) {
         var initial =$(el).height();
@@ -14,34 +15,29 @@ App.run(function($window, $timeout, $rootScope) {
         if (el.offsetParent == el.parentElement) return getOffset(el.parentElement, par) + el.offsetTop;
         return getOffset(el.parentElement, par) + el.offsetTop - el.parentElement.offsetTop;
     }
+    
     function reAlignEls() {
-        if(timeout != undefined) {
-            $timeout.cancel(timeout);
-        }
-        var timeout = $timeout(function() {
-            var bottomAlignedEls = $('.align-parent-bottom');
-            for (var i = 0; i < bottomAlignedEls.length; i++) {
-                var el = bottomAlignedEls[i];
-                var par = findNextParent(el);
-                var dy = getOffset(el, par);
-                if(dy == NaN || dy < 0) dy = 0;
-                var h = $(par).height() - dy;
-                if (h > 0 && $(el).height() != h) {
-                    var discount = el.getAttribute("discount-height");
-                    if (discount == undefined) {
-                        discount = 0;
-                    }
-                    else {
-                        discount = parseInt(discount);
-                    }
-                    $(el).height(h - discount);
+        var bottomAlignedEls = $('.align-parent-bottom');
+        for (var i = 0; i < bottomAlignedEls.length; i++) {
+            var el = bottomAlignedEls[i];
+            var par = findNextParent(el);
+            var dy = getOffset(el, par);
+            if(dy == NaN || dy < 0) dy = 0;
+            var h = $(par).height() - dy;
+            if (h > 0 && $(el).height() != h) {
+                var discount = el.getAttribute("discount-height");
+                if (discount == undefined) {
+                    discount = 0;
                 }
+                else {
+                    discount = parseInt(discount);
+                }
+                $(el).height(h - discount);
             }
-        });
+        }
     }
 
-    $($window).resize(reAlignEls);
+    $($window).resize(_.debounce(reAlignEls, 500));
 
     $rootScope.$on('trigger-resize', reAlignEls);
-    //reAlignEls();
 });
